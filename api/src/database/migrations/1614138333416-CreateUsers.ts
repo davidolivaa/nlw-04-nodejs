70e50e9312bf153bccf239edea66aac3c6887950
David Oliveira Silva <davidoliva96@gmail.com>
2021-02-24 00:48:32 -0300

    Commit criação migrations

---
+++ api/src/database/migrations/1614138333416-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1614138333416 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
