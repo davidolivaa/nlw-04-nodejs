8e7df68007c19b7f0c21d196f47a5992b95e16be
David Oliveira Silva <davidoliva96@gmail.com>
2021-02-24 18:32:35 -0300

    Commit aula 2

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,16 +2,20 @@ import 'reflect-metadata'
 import express from 'express' // os tres pontinhos significam que a tipagem da biblioteca esta em outra bibliteca
 // import nao é entendido pelo node, se rodar o arquivo da erro
 import "./database"
+import { router } from './routes';
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json({message: 'Hello World - NLW04'});
-});
+// app.get('/', (request, response) => {
+//     return response.json({message: 'Hello World - NLW04'});
+// });
 
-app.post('/', (request, response) => {
-    // recebeu dados pra salvar
-    return response.json({message: "Os dados foram salvos com sucesso"})
-});
+// app.post('/', (request, response) => {
+//     // recebeu dados pra salvar
+//     return response.json({message: "Os dados foram salvos com sucesso"})
+// });
+
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => console.log('Server is running'))
