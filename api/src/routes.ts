155dec169ac6f4a9910db2694870d48a2327ca40
David Oliveira Silva <davidoliva96@gmail.com>
2021-02-28 21:35:27 -0300

    commit aula 4

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,15 +3,20 @@ import { Router } from 'express';
 // importaçao dos controles
 import { UserController } from './controllers/UserController'; 
 import { SurveysController } from './controllers/SurveysController';
+import { SendMailController } from './controllers/SendMailController'
 
 const router = Router();
 
 const userController = new UserController(); // na var userController é armazenado um objeto criado com base na class UserController que esta no arquivo UserController.ts
 const surveysController = new SurveysController();
 
+const sendMailController = new SendMailController()
+
 router.post('/users', userController.create); // requisiçao criada com post e parametros enviados
 
 router.post('/surveys', surveysController.create);
 router.get('/surveys', surveysController.show);
+
+router.post("/sendMail", sendMailController.execute)
 
 export { router };
